1d724d5d0ebf1b8bb2c7af55ed7a3fab4e1dccbd
leidiane <leidiane.igtu@gmail.com>
2024-11-24 13:39:25 -0300

    feature: deploy

--- imersao-dev-back-end/insta-back/src/controllers/postsController.js
+++ imersao-dev-back-end/insta-back/src/controllers/postsController.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { atualizarPost, buscarTodosOsPosts, criarPost } from "../models/postsModel.js";
+import gerarDescricaoComGemini from '../services/geminiService.js';
 
 export async function listarPosts(req, res) {
     const posts = await buscarTodosOsPosts(); // Busca os posts
@@ -10,7 +11,7 @@ export async function postarNovoPost(req, res) {
     const novoPost = req.body; // Obtem o novo post da requisição
     try {
         const postCriado = await criarPost(novoPost);
-        res.status(201).json(postCriado);// Envia o novo post como resposta
+        res.status(200).json(postCriado);// Envia o novo post como resposta
     } catch (erro) {
         console.error('Erro ao criar o post:', erro);
         res.status(500).json({ erro: 'Erro ao criar o post' });
@@ -27,7 +28,7 @@ export async function uploadImagem(req, res) {
         const postCriado = await criarPost(novoPost); // Cria um novo post no banco de dados
         const imagemAtualizada = `uploads/${postCriado.insertedId}.png`; // Define o caminho para a imagem com o ID do post
         fs.renameSync(req.file.path, imagemAtualizada); // Renomeia o arquivo para incluir o ID do post
-        res.status(201).json(postCriado); // Envia o novo post como resposta
+        res.status(200).json(postCriado); // Envia o novo post como resposta
     } catch (erro) {
         console.error('Erro ao criar o post:', erro); // Loga erro no console
         res.status(500).json({ erro: 'Erro ao criar o post' }); // Retorna erro ao cliente
@@ -37,14 +38,16 @@ export async function uploadImagem(req, res) {
 export async function atualizarNovoPost(req, res) {
     const id = req.params.id; // Obtem o novo post da requisição
     const urlImagem = `http://localhost:3000/${id}.png`;
-    const post = {
-        imgUrl: urlImagem,
-        descricao: req.body.descricao,
-        alt: req.body.alt
-    }; // Obtem o novo post da requisição
     try {
-        const postCriado = await atualizarPost(id, post);
-        res.status(201).json(postCriado);// Envia o novo post como resposta
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`);// Le o buffer da imagem
+        const descricao = await gerarDescricaoComGemini(imgBuffer);// Gera a descricao com o gemini
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: descricao
+        }; // Obtem o novo post da requisição
+        const postCriado = await atualizarPost(id, post); // Atualiza o post no banco de dados
+        res.status(200).json(postCriado);// Envia o novo post como resposta
     } catch (erro) {
         console.error('Erro ao atualizar o post:', erro);
         res.status(500).json({ erro: 'Erro ao atualizar o post' });
--- imersao-dev-back-end/insta-back/src/routes/postsRoutes.js
+++ imersao-dev-back-end/insta-back/src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from 'express'; // Importa o framework Express
 import multer from 'multer';
+import cors from 'cors';
 import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from '../controllers/postsController.js';
+
+const corsOptions = {
+    origin: 'http://localhost:8000',
+    optionsSuccessStatus: 200,
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -14,6 +20,7 @@ const storage = multer.diskStorage({
 const upload = multer({ dest: "./uploads", storage });
 const routes = (app) => {
     app.use(express.json());// Habilita o parsing de JSON
+    app.use(cors(corsOptions));
     app.get('/posts', listarPosts);// Rota para buscar todos os posts
     app.post('/posts', postarNovoPost);// Rota para criar um novo post
     app.post('/upload', upload.single("imagem"), uploadImagem);
